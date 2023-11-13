14a28fcf827fc377730f144372ef0e39ca549c1c
sanchez-alyssa <ww7aly@gmail.com>
2023-11-13 14:35:06 +0800

    add animation bg

--- app.js
+++ app.js
@@ -1,82 +1,76 @@
 //firebase
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import {
-	getDatabase,
-	ref,
-	onValue,
-	push,
-} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, onValue, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 let appSettings = {
-	databaseURL:
-		'https://champions-1689f-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    databaseURL: "https://champions-1689f-default-rtdb.asia-southeast1.firebasedatabase.app/",
 }
 //set up firebase
 let app = initializeApp(appSettings)
 let database = getDatabase(app)
-let championsInDB = ref(database, 'champions')
+let championsInDB = ref(database, "champions")
 
 //dom
-let publishBtn = document.getElementById('publish-btn')
-let inputEl = document.getElementById('text-area')
-let postEl = document.getElementById('post-el')
-let toPerson = document.getElementById('to-person')
-let fromPerson = document.getElementById('from-person')
+let publishBtn = document.getElementById("publish-btn")
+let inputEl = document.getElementById("text-area")
+let postEl = document.getElementById("post-el")
+let toPerson = document.getElementById("to-person")
+let fromPerson = document.getElementById("from-person")
 
-publishBtn.addEventListener('click', function () {
-	//creating object to push multiple data into firebase
-	let postData = {
-		message: inputEl.value,
-		to: toPerson.value,
-		from: fromPerson.value,
-	}
+publishBtn.addEventListener("click", function () {
+    //creating object to push multiple data into firebase
+    let postData = {
+        message: inputEl.value,
+        to: toPerson.value,
+        from: fromPerson.value,
+    }
 
-	if (inputEl.value && toPerson.value && fromPerson.value) {
-		push(championsInDB, postData) //push data object into firebase
-		clearInputValue() //clear text input
-	}
+    if (inputEl.value && toPerson.value && fromPerson.value) {
+        push(championsInDB, postData) //push data object into firebase
+        clearInputValue() //clear text input
+    }
 })
 //fetch database
 onValue(championsInDB, function (snapshot) {
-	//snapshot.exists - verify if true or false in order to delete last one in database
-	if (snapshot.exists()) {
-		clearPostElement() //need to clear element to prevent adding old value
+    //snapshot.exists - verify if true or false in order to delete last one in database
+    if (snapshot.exists()) {
+        clearPostElement() //need to clear element to prevent adding old value
 
-		//Object.values - is the value in the database
-		let messageArray = Object.values(snapshot.val()) //need to transform to an array
-		messageArray.reverse() //make the value push to the beginning
-		for (let i = 0; i < messageArray.length; i++) {
-			let currentMessage = messageArray[i]
+        //Object.values - is the value in the database
+        let messageArray = Object.values(snapshot.val()) //need to transform to an array
+        messageArray.reverse() //make the value push to the beginning
+        for (let i = 0; i < messageArray.length; i++) {
+            let currentMessage = messageArray[i]
 
-			appendToPostElement(currentMessage) //display the input value
-		}
-	}
+            appendToPostElement(currentMessage) //display the input value
+        }
+    }
 })
 
 function appendToPostElement(message) {
-	let capitalizedTo = capitalizeFirstLetter(message.to)
-	let capitalizedFrom = capitalizeFirstLetter(message.from)
+    let capitalizedTo = capitalizeFirstLetter(message.to) //from postData
+    let capitalizedFrom = capitalizeFirstLetter(message.from) //from postData
 
-	let listItem = document.createElement('li')
-	listItem.innerHTML = `
+    let listItem = document.createElement("li")
+    listItem.innerHTML = `
     <p class="from-and-to">To: ${capitalizedTo}</p>
     <p class="message">${message.message}</p>
     <p class="from-and-to">From: ${capitalizedFrom}</p>
   `
 
-	postEl.appendChild(listItem)
+    postEl.appendChild(listItem)
 }
 //make the first letter always capital
 function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1)
+    return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 function clearPostElement() {
-	postEl.innerHTML = ''
+    postEl.innerHTML = ""
 }
 
 function clearInputValue() {
-	inputEl.value = ''
-	toPerson.value = ''
-	fromPerson.value = ''
+    inputEl.value = ""
+    toPerson.value = ""
+    fromPerson.value = ""
 }
